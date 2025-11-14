c572123699e0397e4913e1d0db4a07f2646d5315
cloudwi <cloudwiiiii@gmail.com>
2025-11-14 17:21:07 +0900

    feat: 모든 환경에서 Sentry 활성화
    - development 환경에서도 Sentry 에러 모니터링 활성화
    - 환경별로 샘플링 비율 조정 (dev: 50% events, 25% traces)
    - config.environment로 환경 구분 추가
    - Sentry trace meta 태그를 모든 환경에 포함

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,11 +1,17 @@
-# Only enable Sentry in production to avoid SSL certificate issues in development
-if Rails.env.production?
-  Sentry.init do |config|
-    config.dsn = 'https://24752814bd022ec107393b16ed310ff5@o4510362292781056.ingest.us.sentry.io/4510362308509696'
-    config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+Sentry.init do |config|
+  config.dsn = 'https://24752814bd022ec107393b16ed310ff5@o4510362292781056.ingest.us.sentry.io/4510362308509696'
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
-    # Add data like request headers and IP for users,
-    # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
-    config.send_default_pii = true
+  # Add data like request headers and IP for users,
+  # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
+  config.send_default_pii = true
+
+  # Set environment
+  config.environment = Rails.env
+
+  # In development, send fewer events to avoid noise
+  if Rails.env.development?
+    config.sample_rate = 0.5 # 50% of events
+    config.traces_sample_rate = 0.25 # 25% of traces
   end
 end
