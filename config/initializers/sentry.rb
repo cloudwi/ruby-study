b05de587490507e00da5d26c749ad6d93df56e64
cloudwi <cloudwiiiii@gmail.com>
2025-11-14 17:37:31 +0900

    fix: Sentry SSL 인증서 에러 해결 및 샘플링 비율 조정
    - development 환경에서 SSL 검증 비활성화
    - Auth0와 동일한 방식으로 Net::HTTP SSL 검증 우회
    - development 샘플링 비율을 100%로 증가 (테스트 용이)
    - SSL certificate verify failed 에러 해결

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,3 +1,21 @@
+# Disable SSL verification in development to avoid certificate issues
+if Rails.env.development?
+  require 'net/http'
+
+  module Net
+    class HTTP
+      alias_method :original_use_ssl=, :use_ssl= unless method_defined?(:original_use_ssl=)
+
+      def use_ssl=(flag)
+        self.original_use_ssl = flag
+        if flag
+          self.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        end
+      end
+    end
+  end
+end
+
 Sentry.init do |config|
   config.dsn = 'https://24752814bd022ec107393b16ed310ff5@o4510362292781056.ingest.us.sentry.io/4510362308509696'
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
@@ -9,9 +27,9 @@ Sentry.init do |config|
   # Set environment
   config.environment = Rails.env
 
-  # In development, send fewer events to avoid noise
+  # In development, send all events for testing
   if Rails.env.development?
-    config.sample_rate = 0.5 # 50% of events
-    config.traces_sample_rate = 0.25 # 25% of traces
+    config.sample_rate = 1.0 # 100% of events
+    config.traces_sample_rate = 1.0 # 100% of traces
   end
 end
