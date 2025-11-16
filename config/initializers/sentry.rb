085d6f42fdf6b5908062bd9298621aaf9021dfb6
cloudwi <cloudwiiiii@gmail.com>
2025-11-17 08:56:15 +0900

    refactor: simplify Sentry configuration
    - Disable Sentry by default in development due to SSL certificate issues
    - Allow opt-in via SENTRY_DSN environment variable
    - Automatically enable in production
    - Remove SSL bypass code for cleaner implementation

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,35 +1,19 @@
-# Disable SSL verification in development to avoid certificate issues
-if Rails.env.development?
-  require 'net/http'
+# Sentry configuration
+# In development, Sentry is disabled due to SSL certificate issues
+# Set SENTRY_DSN environment variable in production
 
-  module Net
-    class HTTP
-      alias_method :original_use_ssl=, :use_ssl= unless method_defined?(:original_use_ssl=)
-
-      def use_ssl=(flag)
-        self.original_use_ssl = flag
-        if flag
-          self.verify_mode = OpenSSL::SSL::VERIFY_NONE
-        end
-      end
-    end
+if Rails.env.production? || ENV['SENTRY_DSN'].present?
+  Sentry.init do |config|
+    config.dsn = ENV['SENTRY_DSN'] || 'https://f573ec547d71c09defb67758c7931bec@o4510362292781056.ingest.us.sentry.io/4510377298493440'
+    config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+    config.send_default_pii = true
+    config.environment = Rails.env
+    config.sample_rate = 1.0
+    config.traces_sample_rate = 1.0
   end
-end
 
-Sentry.init do |config|
-  config.dsn = 'https://24752814bd022ec107393b16ed310ff5@o4510362292781056.ingest.us.sentry.io/4510362308509696'
-  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
-
-  # Add data like request headers and IP for users,
-  # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
-  config.send_default_pii = true
-
-  # Set environment
-  config.environment = Rails.env
-
-  # In development, send all events for testing
-  if Rails.env.development?
-    config.sample_rate = 1.0 # 100% of events
-    config.traces_sample_rate = 1.0 # 100% of traces
-  end
+  puts "✓ Sentry enabled for #{Rails.env} environment"
+else
+  puts "✗ Sentry disabled in #{Rails.env} (SSL certificate issues)"
+  puts "  To enable: export SENTRY_DSN='your-dsn-here'"
 end
